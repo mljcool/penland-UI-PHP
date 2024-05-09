07e13d027ff6579f1180b36beb99abe339c1557c
MGocela <alecogkram@gmail.com>
2024-05-10 01:19:26 +0800

    refine the process

--- scripts/APPLICATION-REQUEST.js
+++ scripts/APPLICATION-REQUEST.js
@@ -1,72 +1,88 @@
-function callAPI() {
-  const stepFourSaveWorkShopItem = async () => {
-    const data = workShopItemsPayload();
-    try {
-      for (let i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        await fetchPostRequest(data[i], ADD_WORSHOPS);
+function startRegistrationProcess() {
+   const stepFourSaveWorkShopItem = async () => {
+      const data = workShopItemsPayload();
+      try {
+         for (let i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            await fetchPostRequest(data[i], ADD_WORSHOPS);
+            afterEachCallStoploadingBySection('workshops');
+            finalMessageSuccess();
+         }
+         console.log('All API calls finished.');
+      } catch (error) {
+         console.error('Error occurred:', error);
       }
-      console.log("All API calls finished.");
-    } catch (error) {
-      console.error("Error occurred:", error);
-    }
-  };
+   };
 
-  const stepsThreeSavePaymentDetails = () => {
-    asyncTask(1, "sample").then(() => {
-      stepFourSaveWorkShopItem();
-    });
-  };
+   const stepsThreeSavePaymentDetails = () => {
+      asyncTask(1, 'sample').then(() => {
+         stepFourSaveWorkShopItem();
+         afterEachCallStoploadingBySection('payments');
+         smoothSrollToNextBlock('workshops');
+      });
+   };
 
-  const saveMemberList = async () => {
-    const data = membersPayload();
-    try {
-      for (let i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        await fetchPostRequest(data[i], ADD_HOUSING_MEMBERS);
+   const saveMemberList = async () => {
+      const data = membersPayload();
+      try {
+         for (let i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            await fetchPostRequest(data[i], ADD_HOUSING_MEMBERS);
+         }
+         stepsThreeSavePaymentDetails();
+         afterEachCallStoploadingBySection('housing');
+         smoothSrollToNextBlock('payments');
+
+         console.log('All API calls finished.');
+      } catch (error) {
+         console.error('Error occurred:', error);
       }
-      stepsThreeSavePaymentDetails();
-      console.log("All API calls finished.");
-    } catch (error) {
-      console.error("Error occurred:", error);
-    }
-  };
+   };
 
-  const stepsTwoAddHousing = () => {
-    const payload = housingPayload();
-    $.ajax({
-      ...requestOptions(ADD_HOUSING, payload),
-      success: function (data) {
-        console.log("Request 2 completed: ");
-        saveMemberList();
-      },
-      error: function () {},
-    });
-  };
+   const stepsTwoAddHousing = () => {
+      const payload = housingPayload();
+      $.ajax({
+         ...requestOptions(ADD_HOUSING, payload),
+         success: function (data) {
+            console.log('Request 2 completed: ');
+            saveMemberList();
+         },
+         error: function () {},
+      });
+   };
 
-  const registerApplication = () => {
-    const payload = applicationPayload();
-    $.ajax({
-      ...requestOptions(REGISTER_APPLICATION, payload),
-      success: function (data) {
-        console.log("Request 1 completed: ");
-        stepsTwoAddHousing();
-      },
-      error: function () {},
-    });
-  };
+   const registerApplication = () => {
+      const payload = applicationPayload();
+      $.ajax({
+         ...requestOptions(REGISTER_APPLICATION, payload),
+         success: function (data) {
+            console.log('Request 1 completed: ');
+            stepsTwoAddHousing();
+         },
+         error: function () {},
+      });
+   };
 
-  const stepOneRegisterAccount = () => {
-    const payload = personalInfoPayload();
-    $.ajax({
-      ...requestOptions(REGISTER_ACCOUNT, payload),
-      success: function (data) {
-        console.log("Request 1 completed: ");
-        stepsTwoAddHousing();
-      },
-      error: function () {},
-    });
-  };
+   const stepOneRegisterAccount = () => {
+      const payload = personalInfoPayload();
+      $.ajax({
+         ...requestOptions(REGISTER_ACCOUNT, payload),
+         success: function (data) {
+            console.log('Request 1 completed: ');
+            afterEachCallStoploadingBySection('personal');
+            smoothSrollToNextBlock('housing');
+            stepsTwoAddHousing();
+         },
+         error: function () {},
+      });
+   };
 
-  stepOneRegisterAccount();
+   const checkAsIs = () => {
+      asyncTask(1, 'sample').then(() => {
+        afterEachCallStoploadingBySection('account');
+        smoothSrollToNextBlock('personal');
+         stepOneRegisterAccount();
+      });
+   };
+   checkAsIs();
 }
--- scripts/PROCESS-DATA.js
+++ scripts/PROCESS-DATA.js
@@ -1,147 +1,156 @@
-var timelines = ["account", "personal", "housing", "payments", "workshops"];
+var timelines = ['account', 'personal', 'housing', 'payments', 'workshops'];
 let message = {
-  account: `Your account save successfully! 👍`,
-  personal: `Your personal information save successfully! 👍`,
-  housing: `Your housing save successfully! 👍`,
-  payments: `Your payments save successfully! 👍`,
-  workshops: `Your workshops save successfully! 👍`,
+   account: `Your account save successfully! 👍`,
+   personal: `Your personal information save successfully! 👍`,
+   housing: `Your housing save successfully! 👍`,
+   payments: `Your payments save successfully! 👍`,
+   workshops: `Your workshops save successfully! 👍`,
 };
 
 function loadingBlockUI(propBody) {
-  $(".body-timeline-" + propBody).block({
-    message: HTMLelementProp.blockUISummaryElement,
-    css: {
-      backgroundColor: "transparent",
-      border: "0",
-    },
-    overlayCSS: {
-      backgroundColor: "#fff",
-      opacity: 0.8,
-    },
-  });
+   $('.body-timeline-' + propBody).block({
+      message: HTMLelementProp.blockUISummaryElement,
+      css: {
+         backgroundColor: 'transparent',
+         border: '0',
+      },
+      overlayCSS: {
+         backgroundColor: '#fff',
+         opacity: 0.8,
+      },
+   });
 }
 
 function loadingBlockUISmile(propBody) {
-  $(".body-timeline-" + propBody).block({
-    message: HTMLelementProp.blockUIExistELementSmile(message[propBody]),
-    css: {
-      backgroundColor: "transparent",
-      border: "0",
-      cursor: "default",
-    },
-    overlayCSS: {
-      backgroundColor: "#fff",
-      opacity: 0.9,
-      cursor: "default",
-    },
-  });
+   $('.body-timeline-' + propBody).block({
+      message: HTMLelementProp.blockUIExistELementSmile(message[propBody]),
+      css: {
+         backgroundColor: 'transparent',
+         border: '0',
+         cursor: 'default',
+      },
+      overlayCSS: {
+         backgroundColor: '#fff',
+         opacity: 0.9,
+         cursor: 'default',
+      },
+   });
 }
 
-function smoothSroll(value) {
-  $("html, body").animate(
-    {
-      scrollTop: $(".wrapper-timeline-" + value).offset().top,
-    },
-    300
-  );
-}
+
 
 // Function to simulate an asynchronous task with a delay
 function asyncTask(index, value) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(value);
-    }, Math.random() * 5000); // Simulating a random delay
-  });
+   return new Promise((resolve) => {
+      setTimeout(() => {
+         resolve(value);
+      }, Math.random() * 5000); // Simulating a random delay
+   });
 }
 
 // Sequential promise calls using async/await
 async function sequentialPromiseCalls(timelines) {
-  console.log("Starting sequential promise calls...");
-  const results = [];
+   console.log('Starting sequential promise calls...');
+   const results = [];
 
-  try {
-    for (let [index, task] of timelines.entries()) {
-      const result = await asyncTask(index, task).then((response) => {
-        console.log("response:", response);
+   try {
+      for (let [index, task] of timelines.entries()) {
+         const result = await asyncTask(index, task).then((response) => {
+            console.log('response:', response);
 
-        setCheckIcon(response);
-        $(".body-timeline-" + response).unblock();
-        loadingBlockUISmile(response);
+            setCheckIcon(response);
+            $('.body-timeline-' + response).unblock();
+            loadingBlockUISmile(response);
 
-        if (index < timelines.length) {
-          setTimeout(() => {
-            const sectionScroll = timelines[index + 1];
-            if (sectionScroll) {
-              smoothSroll(sectionScroll);
+            if (index < timelines.length) {
+               setTimeout(() => {
+                  const sectionScroll = timelines[index + 1];
+                  if (sectionScroll) {
+                     smoothSrollToNextBlock(sectionScroll);
+                  }
+               }, 1000);
             }
-          }, 1000);
-        }
-        if (response === "workshops") {
-          setTimeout(() => {
-            messageSuccess();
-          }, 1200);
-        }
-      });
-      results.push(result);
-    }
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+            if (response === 'workshops') {
+               setTimeout(() => {
+                  finalMessageSuccess();
+               }, 1200);
+            }
+         });
+         results.push(result);
+      }
+   } catch (error) {
+      console.error('An error occurred:', error);
+   }
 }
 
 function setCheckIcon(_data) {
-  $(".icon-timeline-" + _data).html(
-    `<i class='bx bx-check-circle color-success fa-2x' ></i>`
-  );
+   $('.icon-timeline-' + _data).html(
+      `<i class='bx bx-check-circle color-success fa-2x' ></i>`
+   );
 }
+
+function smoothSrollToNextBlock(value) {
+    $('html, body').animate(
+       {
+          scrollTop: $('.wrapper-timeline-' + value).offset().top,
+       },
+       300
+    );
+ }
 
 function extraScrollSmooth(value) {
-  $("html, body").animate(
-    {
-      scrollTop: $("." + value).offset().top,
-    },
-    900
-  );
+   $('html, body').animate(
+      {
+         scrollTop: $('.' + value).offset().top,
+      },
+      900
+   );
 }
 
-function messageSuccess() {
-  $(".wrapper-timeline-success").css("display", "block");
-  extraScrollSmooth("wrapper-timeline-success");
+function finalMessageSuccess() {
+   $('.wrapper-timeline-success').css('display', 'block');
+   extraScrollSmooth('wrapper-timeline-success');
 
-  setTimeout(() => {
-    $(".message-success").show("slow").slideDown();
-    setTimeout(() => {
-      extraScrollSmooth("success-wrapper-body");
-    }, 600);
-  }, 600);
+   setTimeout(() => {
+      $('.message-success').show('slow').slideDown();
+      setTimeout(() => {
+         extraScrollSmooth('success-wrapper-body');
+      }, 600);
+   }, 600);
 }
 
 function fireDummyAsyncCall() {
-  extraScrollSmooth("focus-banner-container");
-  sequentialPromiseCalls(timelines);
-  timelines.forEach((_data) => {
-    loadingBlockUI(_data);
-    $(".icon-timeline-" + _data).html(
-      `<i class="bx bx-loader bx-spin fa-2x"></i>`
-    );
-  });
-  $(".footer-btns").css("display", "none");
+   extraScrollSmooth('focus-banner-container');
+   sequentialPromiseCalls(timelines);
+   timelines.forEach((_data) => {
+      loadingBlockUI(_data);
+      $('.icon-timeline-' + _data).html(
+         `<i class="bx bx-loader bx-spin fa-2x"></i>`
+      );
+   });
+   $('.footer-btns').css('display', 'none');
 }
 
+function afterEachCallStoploadingBySection(sectionClass) {
+   $('.body-timeline-' + sectionClass).unblock();
+   setCheckIcon(sectionClass);
+   loadingBlockUISmile(sectionClass);
+}
 
+function startAddLoadingEachSections() {
+   extraScrollSmooth('focus-banner-container');
+   timelines.forEach((_data) => {
+      loadingBlockUI(_data);
+      $('.icon-timeline-' + _data).html(
+         `<i class="bx bx-loader bx-spin fa-2x"></i>`
+      );
+   });
+   //    $('.footer-btns').css('display', 'none');
+}
 
 $(document).ready(function () {
-  $(".final-button-steps").click(function () {
-    // callAPI();
-    // console.log(workShopItemsPayload());
-    console.log(housingPayload());
-    // https://jsonplaceholder.typicode.com/posts
-    // console.log(membersPayload());
-    // console.log(applicationPayload());
-    // const workshopsPayload = applicationPayload();
-    // testApplication(workshopsPayload);
-    // const workshopsPayload = workShopItemsPayload();
-    // sequentialAPICallsTest(workShopItemsPayload());
-  });
+   $('.final-button-steps').click(function () {
+      startAddLoadingEachSections();
+      startRegistrationProcess();
+   });
 });
